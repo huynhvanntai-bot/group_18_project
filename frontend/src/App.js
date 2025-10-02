23a4f0645811da9bd10c1b4f45e9d70559b00a0d
phamquanghuy1661 <phamquanghuysoctrang@gmail.com>
2025-10-02 16:19:24 +0700

    Hoàn thành bài tập quản lý user

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import UserList from "./UserList";
 import AddUser from "./AddUser";
 
 function App() {
+  const [users, setUsers] = useState([
+    { id: 1, name: "Nguyễn Văn A" },
+    { id: 2, name: "Trần Thị B" }
+  ]);
+
+  const addUser = (name) => {
+    const newUser = { id: Date.now(), name };
+    setUsers([...users, newUser]);
+  };
+
   return (
-    <div>
+    <div style={{ padding: "20px" }}>
       <h1>Quản lý User</h1>
-      <AddUser />
-      <UserList />
+      <AddUser onAddUser={addUser} />
+      <UserList users={users} />
     </div>
   );
 }
